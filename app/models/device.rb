20a90a7283bdd8b0c11632cc6225d5328fc639b8
holloway <hollowaykeanho@gmail.com>
2015-12-29 15:34:01 +0800

    created question for device model
    Signed-off-by: holloway <hollowaykeanho@gmail.com>

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,2 @@
 class Device < ActiveRecord::Base
-	default_scope { order('created_at DESC') }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151229033508) do
+ActiveRecord::Schema.define(version: 20151229031306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "devices", force: :cascade do |t|
-    t.string   "name"
-    t.string   "address"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
