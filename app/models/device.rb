a70ea1dca03643302efdfd327ea9c484b49b2010
Leo Koo <leokoo@gmail.com>
2015-12-29 18:22:38 +0800

    fixed all errors

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,19 @@
 class Device < ActiveRecord::Base
-	has_many :user
+	has_many :users
+	default_scope { order('id DESC') }
+
+	def name=(value)
+    raise ArgumentError unless value.is_a?(String)
+    raise ArgumentError unless value.length >= 4
+    raise ArgumentError unless value.length <= 16
+    super
+	end
+
+def address=(value)
+    #^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$
+    raise ArgumentError unless value.is_a?(String)
+    raise ArgumentError unless /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/ =~ value
+    super
+  end
+
 end
